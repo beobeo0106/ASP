9bd57d53d4e57ffd27b5a27ba5a3570ae93aa7b4
Admin <Admin@D20901>
2026-03-06 14:35:58 +0700

    aaaaa

--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -22,6 +22,7 @@ public class WeatherForecastController : ControllerBase
     public IEnumerable<WeatherForecast> Get()
     {
         //lê như thảo
+        //phu thái
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
